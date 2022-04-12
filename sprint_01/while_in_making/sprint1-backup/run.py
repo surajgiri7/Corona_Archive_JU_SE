ce74504b0e53b9663ddeadbf274c47349a953ac6
Suraj Giri <surajgiri4210@gmail.com>
2022-04-12 19:33:02 +0200

    All the works Until third Sprint

--- sprint_01/while_in_making/sprint1-backup/run.py
+++
@@ -1,6 +0,0 @@
-from corona import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
-#this file is used for running the program
--- sprint_01/while_in_making/sprint_01(backup)/run.py
+++
@@ -1,4 +0,0 @@
-from corona import app
-
-if __name__ == "__main__":
-    app.run(debug=True)
