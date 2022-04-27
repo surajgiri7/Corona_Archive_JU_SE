93a5353b328fd19b611ad846b93325156a21f157
Suraj Giri <63389681+surajgiri7@users.noreply.github.com>
2022-04-27 14:15:46 +0200

    Delete sprint_02/previous_group_17's_work/se-02-team-17 directory

--- sprint_02/previous_group_17's_work/se-02-team-17/agency_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['suser']);
-    session_destroy();
-    header('Location: agency_login.php');
-?>
--- sprint_02/previous_group_17's_work/se-02-team-17/hospital_logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION['huser']);
-    session_destroy();
-    header('Location: hospital_login.php');
-?>
